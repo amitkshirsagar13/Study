7336cb778eeee3f5461d352e95e366c6f5aba731
amit.kshirsagar.13@gmail.com <amit.kshirsagar.13@gmail.com@00bf6314-d853-5732-4094-fd89616d7e70>
2012-04-02 18:54:40 +0000

    Add Role Query

--- JTutor/src/com/JTutor/dao/UserDAO.java
+++ JTutor/src/com/JTutor/dao/UserDAO.java
@@ -10,23 +10,40 @@ import java.util.List;
 import org.apache.log4j.Logger;
 
 import com.JTutor.data.User;
-import com.JTutor.dbConnection.DBConnection;
+import com.JTutor.dbConnection.OracleDBConnection;
+import com.JTutor.front.JTutorMainFrame;
 
 public class UserDAO {
 
 	private static Logger _log = null;
+	private static UserDAO userDAO = null;
 
-	public UserDAO() {
+	private UserDAO() {
 		super();
 		if (_log == null) {
 			_log = Logger.getLogger(UserDAO.class);
 		}
 	}
 
-	public List<User> getUserList() {
-		List<User> userList = new ArrayList<User>();
+	public static void initiateUserDAO() {
+		if (userDAO == null) {
+			userDAO = new UserDAO();
+			logMessage("Initializing UserDAO...");
+		}
+	}
 
-		Connection dbConnection = DBConnection.getDBConnection();
+	private static void logMessage(String message) {
+		JTutorMainFrame.statusBarMsg(message);
+		_log.info(message);
+
+	}
+
+	public static List<User> getUserList() {
+
+		Connection dbConnection = OracleDBConnection.getDBConnection();
+
+		List<User> userList = new ArrayList<User>();
+		logMessage("Getting user list...");
 		String query = "select * from users";
 		Statement stmt;
 		try {
@@ -38,9 +55,33 @@ public class UserDAO {
 				userList.add(new User(rs.getInt(1) + "", rs.getString(2), rs
 						.getInt(3) + ""));
 			} // end while
+
+			logMessage("Got user list...");
 		} catch (SQLException e) {
 			_log.error("Error getting userlist", e);
 		}
 		return userList;
+	}
+
+	public static List<String> getRoleList() {
+		Connection dbConnection = OracleDBConnection.getDBConnection();
+		logMessage("Getting role list...");
+		String query = "select distinct role from users order by 1";
+		List<String> roleList = new ArrayList<String>();
+		Statement stmt;
+		try {
+			stmt = dbConnection.createStatement();
+
+			ResultSet rs = stmt.executeQuery(query);
+
+			while (rs.next()) {
+				roleList.add(rs.getString(1));
+			} // end while
+
+			logMessage("Got role list...");
+		} catch (SQLException e) {
+			_log.error("Error getting rolelist", e);
+		}
+		return roleList;
 	}
 }
