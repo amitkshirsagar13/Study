59ad6fa6a8b29a576a297946573d9c34b0616a48
amit.kshirsagar.13@gmail.com <amit.kshirsagar.13@gmail.com@00bf6314-d853-5732-4094-fd89616d7e70>
2014-01-17 18:01:58 +0000

    Initial CheckIn MasterSwings

---
+++ org/MasterSwings/ApplicationMasterSwings/src/org/masterswings/application/ApplicationMainFrame.java
@@ -0,0 +1,81 @@
+/**
+ * <p>
+ * <b>Overview:</b>
+ * <p>
+ *
+ *
+ * <pre>
+ * Creation date: Jan 17, 2014
+ * @author Amit Kshirsagar
+ * @email amit.kshirsagar.13@gmail.com
+ * @version 1.0
+ * @since
+ *
+ * <p><b>Modification History:</b><p>
+ *
+ *
+ * </pre>
+ */
+
+package org.masterswings.application;
+
+import java.awt.Dimension;
+import java.io.IOException;
+
+import org.apache.log4j.Logger;
+import org.masterswings.base.util.BaseMasterSwingsUtil;
+import org.masterswings.base.view.BaseMasterSwingsFrame;
+
+public class ApplicationMainFrame extends BaseMasterSwingsFrame {
+
+	Logger _log = Logger.getLogger(ApplicationMainFrame.class.getName());
+
+	private void logMessage(String message, Throwable exception) {
+		if (exception != null) {
+			_log.error(message, exception);
+		} else {
+			_log.info(message);
+		}
+	}
+
+	private void debug(String message) {
+		_log.debug(message);
+	}
+
+	/**
+	 * @param applicationName
+	 * @throws IOException
+	 */
+	public ApplicationMainFrame(String applicationName) throws IOException {
+		super(applicationName);
+		loadApplicationProperties();
+		this.setSize(new Dimension(BaseMasterSwingsUtil
+				.getIntegerFromString(_properties.getProperty(APPWIDTH)),
+				BaseMasterSwingsUtil.getIntegerFromString(_properties
+						.getProperty(APPHEIGHT))));
+		this.setLocation(BaseMasterSwingsUtil.getIntegerFromString(_properties
+				.getProperty(XAPP)), BaseMasterSwingsUtil
+				.getIntegerFromString(_properties.getProperty(YAPP)));
+		initialize();
+	}
+
+	/**
+	 * Start Loading the Componants on mainFrame...
+	 */
+
+	@Override
+	public void initialize() {
+
+		for (int index = 0; index < 11; index++) {
+			_splashScreen.setProgress("Initilizing GUI..." + index, index * 10);
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		_splashScreen.closeIt();
+	}
+}
